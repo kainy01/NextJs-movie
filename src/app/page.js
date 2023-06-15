81469d61a2d813aadd9259b2a2290f87cb147306
alwaysalty <cenkerdonmez@gmail.com>
2023-06-15 20:35:41 +0300

    a

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,50 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
+const queryClient = new QueryClient();
+
+export default function App() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
+  );
+}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+function Example() {
+  const [search, setSearch] = useState();
+  const [nominate, setNominate] = useState();
+  const [resault, setResault] = useState();
+  function handleInput(event) {
+    setSearch(event.target.value);
+  }
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch(`http://www.omdbapi.com/?s=${search}&apikey=99661966`).then((res) =>
+      res.json()
+    )
+  );
+  let searchData= data.Search
+  function handleNominate(searchData){
+    setNominate([...nominate, searchData])
+  }
+  console.log(nominate)
+  if (isLoading) return "Loading...";
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+  if (error) return "An error has occurred: " + error.message;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+  return (
+    <div>
+      <input onChange={handleInput} type="text" value={search}></input>
+      <p>
+        {data.Search.slice(0, 3).map((item) => (
+          <p key={item.imdbID}>
+            {item.Title} - {item.Year}- {item.Type}{" "}
+            <button onClick={handleNominate}>Nominate</button>{" "}
           </p>
-        </a>
-      </div>
-    </main>
-  )
+        ))}{" "}
+      </p>
+    </div>
+  );
 }
