59d13a01d67c5e13439ce70f871a4d8888fa47e1
kainy01 <kainykawai@gmail.com>
2023-06-16 14:02:36 +0300

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
 
-const YourComponent = () => {
+const App = () => {
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [nominate, setNominate] = useState([]);
@@ -28,8 +29,8 @@ const YourComponent = () => {
   const handleInputChange = (event) => {
     setSearch(event.target.value);
   };
-  const handleNominate = (item) => {
 
+  const handleNominate = (item) => {
     setNominate([...nominate, item]);
   };
 
@@ -39,37 +40,105 @@ const YourComponent = () => {
 
   return (
     <>
-      <div>
-        <input type="text" value={search} onChange={handleInputChange} />
-        <p>Search results:</p>
-        {searchResults.length > 0 && search.trim() !== ""
-          ? searchResults.map((item) => (
-              <p key={item.imdbID}>
-                {item.Title}{" "}
-                <button
-                  onClick={() => handleNominate(item)}
-                  disabled={nominate.some(
-                    (movie) =>
-                      movie.imdbID === item.imdbID || nominate.length > 4
-                  )}
+      <div className="container p-lg-5">
+        <h1 className="my-4 fw-bold col-lg-6 col-12">
+          Search below to nominate your top 5 favorite movies & series.
+        </h1>
+        <div className="row">
+          <div className="col-lg-6 col-12">
+            <div className="mb-3">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control shadow-none border-none"
+                  value={search}
+                  onChange={handleInputChange}
+                />
+                <span className="input-group-text searchIcon">
+                  <FaSearch />
+                </span>
+              </div>
+              <h6 className="mt-3 ms-2">Search results:</h6>
+              <ul className="list-group list-group-flush ms-1">
+                {searchResults.length > 0 && search.trim() !== "" ? (
+                  searchResults.map((item) => (
+                    <li
+                      class="list-group-item list-group-item-action list-group-item-primary bg-transparent d-flex justify-content-between align-items-center"
+                      key={item.imdbID}
+                    >
+                      <div className="mt-1">
+                        <h5 className="text-info">{item.Title} </h5>
+                        <p>
+                          {item.Type} &#x2022; {item.Year}
+                        </p>
+                      </div>
+                      <div>
+                        <button
+                          className="btn btn-primary btn-sm"
+                          onClick={() => handleNominate(item)}
+                          disabled={nominate.some(
+                            (movie) =>
+                              movie.imdbID === item.imdbID ||
+                              nominate.length > 4
+                          )}
+                        >
+                          Nominate
+                        </button>
+                      </div>
+                    </li>
+                  ))
+                ) : search.trim() !== "" ? (
+                  <h5 className="text-center text-danger fw-bold">
+                    No movies found.
+                  </h5>
+                ) : null}
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-6 col-12 ">
+            <h2 className="text-center fw-bold">
+              Nominated Movies {nominate.length}/5
+            </h2>
+            <ul className="list-group list-group-flush ms-1 mt-2">
+              {nominate.map((item) => (
+                <li
+                  class="list-group-item list-group-item-action list-group-item-primary bg-transparent d-flex justify-content-between align-items-center"
+                  key={item.imdbID}
                 >
-                  Nominate
-                </button>
-              </p>
-            ))
-          : search.trim() !== "" && <p>No movies found.</p>}
-      </div>
-      <div>
-        <h2>Nominated Movies</h2>
-        {nominate.map((item) => (
-          <p key={item.imdbID}>
-            {item.Title}{" "}
-            <button onClick={() => handleDelete(item.imdbID)}>Remove</button>
-          </p>
-        ))}
+                  <div className="mt-1">
+                    <div className="row d-flex justify-content-start align-items-center">
+                      <div className="col-4 img-container ">
+                        <img
+                          src={item.Poster}
+                          alt={item.Title}
+                        width={50}
+                        height={70}
+                        />
+                      </div>
+                      <div className="col-8">
+                        <h5 className="text-danger">{item.Title} </h5>
+                        <p>
+                          {item.Type} &#x2022; {item.Year}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <button
+                      className="btn btn-danger btn-sm"
+                      onClick={() => handleDelete(item.imdbID)}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
 };
 
-export default YourComponent;
+export default App;
